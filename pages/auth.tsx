fb7cb14cbc3bd82240245031c7523ba9cb11a86a
nuhptr <nugrahaadi733@gmail.com>
2023-09-22 14:42:34 +0700

    pages: auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,11 @@
-import { useCallback, useState } from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
+import axios from 'axios'
+import { useCallback, useState } from 'react'
+import { signIn } from 'next-auth/react'
+
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 import Input from '@/components/input'
 
@@ -15,10 +20,30 @@ export default function Auth() {
     setVariant((currentVariant) => (currentVariant === 'login' ? 'register' : 'login'))
   }, [])
 
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', { email, password, callbackUrl: '/profiles' })
+    } catch (error) {
+      console.error(error)
+    }
+  }, [email, password])
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', { name, email, password })
+    } catch (error) {
+      console.error(error)
+    }
+
+    login()
+  }, [email, login, name, password])
+
+  const title = variant === 'login' ? 'Sign In' : 'Register'
+
   return (
     <>
       <Head>
-        <title>{`${variant === 'login' ? 'Login' : 'Sign Up'} | Netflix`}</title>
+        <title>{`${title} | Netflix`}</title>
         <meta name='description' content='Create / Sign In to netflix' />
       </Head>
 
@@ -35,7 +60,7 @@ export default function Auth() {
           </nav>
 
           <div className='flex justify-center'>
-            <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+            <div className='bg-black bg-opacity-70 px-14 py-10 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
               <h2 className='text-white text-4xl mb-8 font-semibold'>
                 {variant === 'login' ? 'Sign In' : 'Create an account'}
               </h2>
@@ -64,15 +89,30 @@ export default function Auth() {
                   value={password}
                 />
               </div>
-              <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+              <button
+                onClick={variant === 'login' ? login : register}
+                className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
                 {variant === 'login' ? 'Login' : 'Sign Up'}
               </button>
-              <p className='text-neutral-500 mt-11'>
+              {/* Social Auth */}
+              <div className='flex items-center gap-4 mt-8 justify-center'>
+                <div
+                  className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'
+                  onClick={() => signIn('google', { callbackUrl: '/profiles' })}>
+                  <FcGoogle size={30} />
+                </div>
+                <div
+                  className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'
+                  onClick={() => signIn('github', { callbackUrl: '/profiles' })}>
+                  <FaGithub size={30} />
+                </div>
+              </div>
+              <p className='text-neutral-500 mt-8'>
                 {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                 <span
                   onClick={toggleVariant}
                   className='text-white ml-1 hover:underline cursor-pointer'>
-                  {variant === 'login' ? 'Create an account' : 'Sign In'}
+                  {variant === 'login' ? 'Create an account' : 'Login'}
                 </span>
               </p>
             </div>
---
+++ pages/profiles.tsx
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { NextPageContext } from 'next'
+import { getSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import useCurrentUser from '@/hooks/use-current-user'
+
+interface UserCardProps {
+  name: string
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+  if (!session) return { redirect: { destination: '/auth', permanent: false } }
+
+  return { props: {} }
+}
+
+export default function Profiles() {
+  const router = useRouter()
+  const { data: user } = useCurrentUser()
+
+  return (
+    <>
+      <Head>
+        <title>Profiles | Netflix</title>
+      </Head>
+
+      <div className='flex items-center h-full justify-center'>
+        <div className='flex flex-col'>
+          <h1 className='text-3xl md:text-6xl text-white text-center'>Who is watching?</h1>
+          <div className='flex items-center justify-center gap-8 mt-10'>
+            <div onClick={() => router.push('/')}>
+              <div className='group w-44 mx-auto'>
+                <div className='w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
+                  <Image
+                    src={'/images/default-blue.png'}
+                    alt='Profile'
+                    width={150}
+                    height={150}
+                    objectFit='contain'
+                    priority
+                  />
+                </div>
+
+                <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
+                  {user?.name}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
