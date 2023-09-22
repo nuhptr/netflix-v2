cdfc17d2f80169619521c8252e201a98b4ecac11
nuhptr <nugrahaadi733@gmail.com>
2023-09-22 14:40:41 +0700

    helper: add new helpers

---
+++ helpers/server-auth.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+
+import { options } from '@/pages/api/auth/[...nextauth]'
+import prisma from '@/helpers/prismadb'
+
+export default async function serverAuth(request: NextApiRequest, response: NextApiResponse) {
+  const session = await getServerSession(request, response, options)
+  if (!session?.user?.email) throw new Error('Not Signed In!')
+
+  const currentUser = await prisma.user.findUnique({ where: { email: session.user.email } })
+  if (!currentUser) throw new Error('Not Signed In!')
+
+  return currentUser
+}
