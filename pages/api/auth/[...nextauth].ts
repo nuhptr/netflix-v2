495b46008968088634216459be2e2d6000fc63c3
nuhptr <nugrahaadi733@gmail.com>
2023-09-22 14:39:52 +0700

    nextauth: setup update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,24 @@
-import NextAuth, { AuthOptions } from 'next-auth'
 import bcrypt from 'bcrypt'
-import Credentials from 'next-auth/providers/credentials'
+import NextAuth, { AuthOptions } from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
+import Credentials from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
 
 import prisma from '@/helpers/prismadb'
 
 export const options: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
@@ -21,7 +32,7 @@ export const options: AuthOptions = {
         }
 
         const user = await prisma.user.findUnique({ where: { email: credentials.email } })
-        if (!user) throw new Error('Email does not exist')
+        if (!user || !user?.hashedPassword) throw new Error('Email does not exist')
 
         const isValidPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
         if (!isValidPassword) throw new Error('Incorrect Password')
@@ -33,8 +44,9 @@ export const options: AuthOptions = {
   pages: { signIn: '/auth' },
   debug: process.env.NODE_ENV === 'development',
   session: { strategy: 'jwt' },
-  jwt: { secret: process.env.JWT_SECRET },
   secret: process.env.NEXTAUTH_SECRET,
+  // (optional) because deprecated
+  jwt: { secret: process.env.NEXTAUTH_JWT_SECRET },
 }
 
 export default NextAuth(options)
