97982e595e795ae4dc7854f535c992e71527737a
nuhptr <nugrahaadi733@gmail.com>
2023-09-23 22:11:20 +0700

    config: add domain image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
+      'upload.wikimedia.org',
+      'uhdtv.io',
+      'mango.blender.org',
+      'download.blender.org',
+    ],
   },
 }
 
