6b7d30e594cc5ed79b303f6b5e1679818df8c48a
nuhptr <nugrahaadi733@gmail.com>
2023-09-24 14:11:46 +0700

    hooks: update new hooks

---
+++ hooks/use-movie.ts
@@ -0,0 +1,13 @@
+import useSWR from 'swr'
+
+import fetcher from '@/helpers/fetcher'
+
+export default function useMovie(id?: string) {
+  const { data, error, isLoading } = useSWR(id ? `/api/movies/${id}` : null, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  })
+
+  return { data, error, isLoading }
+}
---
+++ hooks/use-movielist.ts
@@ -0,0 +1,13 @@
+import useSWR from 'swr'
+
+import fetcher from '@/helpers/fetcher'
+
+export default function useMovieList() {
+  const { data, error, isLoading } = useSWR('/api/movies', fetcher, {
+    revalidateOnFocus: false,
+    revalidateIfStale: false,
+    revalidateOnReconnect: false,
+  })
+
+  return { data, error, isLoading }
+}
